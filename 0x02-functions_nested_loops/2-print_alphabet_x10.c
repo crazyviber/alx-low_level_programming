41f4fbe42b66ad644f5c36b53a47b11484241819
crazyviber <salamiwahley@yahoo.com>
2023-03-21 08:39:00 -0700

    _x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main function
+ *
+ * Return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	int i;
+
+	while (i <= 9)
+	{
+		for (x = 'a' ; x <= 'z' ; x++)
+	{
+			_putchar(x);
+	}
+	_putchar('\n');
+	i++;
+	}
+}
