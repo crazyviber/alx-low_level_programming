4f3b4ff5cd2650da39b85bf13ed3c26c2241b3ec
crazyviber <salamiwahley@yahoo.com>
2023-03-21 22:20:28 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,10 +11,10 @@ int print_last_digit(int n)
 	int last;
 
 	last = n % 10;
-		if (last < 10)
-		{
-		last = last * -1;
-		}
+	if (last < 0)
+	{
+	last = last * -1;
+	}
 	_putchar(last + '0');
 	return (last);
 }
