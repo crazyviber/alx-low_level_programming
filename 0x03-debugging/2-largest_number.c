4367d1e7b0397316182e600eb04afa5009934b81
crazyviber <salamiwahley@yahoo.com>
2023-03-24 02:23:49 -0700

    large

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,23 +14,16 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
-		largest = a;
-	}
-	else if (a > c && c > c)
-	{
-		largest = a;
+	largest = a;
 	}
 	else if (b > a && a > c)
 	{
-		largest = b;
+	largest = b;
 	}
-	else if (b > c && c > a)
+	else if (c >= a && c >= b)
 	{
-		largest = b;
+	largest = c;
 	}
-	else
-	{
-		largest = c;
-	}
-	return (0)
+
+	return (largest);
 }
