39b0bbb9b6a42af80957eb9f36cb21d3098fd9e2
crazyviber <salamiwahley@yahoo.com>
2023-03-23 17:21:44 -0700

    aa

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-
 /**
 * main - causes an infinite loop
+* @\\o/\\n": parameter a
+* @:(: parameter b
 * Return: 0
 */
 
@@ -11,14 +12,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	/**
-	 * i = 0;
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+	i = 0;
+/**
+* while (i < 10)
+* {
+*       putchar(i);
+*       }
+*/
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
