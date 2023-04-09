858610a9fecd61d5bb38811ed790e3e8f92d81d6
crazyviber <salamiwahley@yahoo.com>
2023-04-09 01:34:31 -0700

    argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function to print out the file name
+ * @argc: agrc parameter
+ * @argv: an array of argc
+ * Return: 0 for success
+ */
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
