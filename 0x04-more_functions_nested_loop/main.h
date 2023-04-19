986c49e124433e603bf87cf07daf9b54e4fdaca3
crazyviber <salamiwahley@yahoo.com>
2023-04-19 08:17:57 -0700

    a

--- 0x04-more_functions_nested_loop/main.h
+++ 0x04-more_functions_nested_loop/main.h
@@ -1,10 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * main - main fonction
- * int prototypes (int)
- * void prototypes(void)
- */
+int _putchar(char);
 int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif /*MAIN_H*/
